f0f68fdb0f61a992df89fbad7b636d7c02d36e16
Trygve <trygveruud@gmail.com>
2021-04-12 16:13:06 +0200

    Revert "3.2.3"
    This reverts commit 5c494603313457844034f43cfbf9b59bdd379be3.

--- Lab 3 - Mikrokontroller/3_gpiote/main.c
+++ Lab 3 - Mikrokontroller/3_gpiote/main.c
@@ -6,29 +6,20 @@
 #define PIN_BUTTON_A 17
 #define PIN_BUTTON_B 26
 
+#define BTN_A GPIO->PIN_CNF[PIN_BUTTON_A]
+#define BTN_B GPIO->PIN_CNF[PIN_BUTTON_B]
+
 int main(){
 	// Configure LED Matrix
-	for(int i = 4; i <= 12; i++){
+	for(int i = 4; i <= 15; i++){
 		GPIO->DIRSET = (1 << i);
 		GPIO->OUTCLR = (1 << i);
 	}
 
-	GPIOTE->CONFIG[0] = (1<<0) | (PIN_BUTTON_A<<8) | (2<<16); // A = Event = 1, B = PIN_A, C = HiToLo = 2
-	GPIOTE->CONFIG[1] = (3<<0) | (13<<8) | (3<<16); // A = Taskt = 3, B = PIN_13, C = Toggle = 3
-	GPIOTE->CONFIG[2] = (3<<0) | (14<<8) | (3<<16); // A = Taskt = 3, B = PIN_14, C = Toggle = 3
-	GPIOTE->CONFIG[3] = (3<<0) | (15<<8) | (3<<16); // A = Taskt = 3, B = PIN_15, C = Toggle = 3
-
-	// Setter på PPI, slik at Btn_A er koblet opp til LED-pins 13, 14, 15
-	PPI->PPI_CH[1].EEP = (uint32_t)&(GPIOTE->IN[0]);
-	PPI->PPI_CH[1].TEP = (uint32_t)&(GPIOTE->OUT[1]);
-
-	PPI->PPI_CH[2].EEP = (uint32_t)&(GPIOTE->IN[0]);
-	PPI->PPI_CH[2].TEP = (uint32_t)&(GPIOTE->OUT[2]);
-
-	PPI->PPI_CH[3].EEP = (uint32_t)&(GPIOTE->IN[0]);
-	PPI->PPI_CH[3].TEP = (uint32_t)&(GPIOTE->OUT[3]);
-
-
+	// Configure buttons
+	BTN_A = 0; // A
+	BTN_B = 0; // B
+	
 	while(1){
 
 
